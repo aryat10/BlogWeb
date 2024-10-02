a2f9bb2c6b55c1904e92ed2f1f89de80856c2329
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2024-10-02 22:21:58 +0530

    Update db.js

--- Server/Database/db.js
+++ Server/Database/db.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
   // This will establish a conection between the database
   const URL = `mongodb://${username}:${password}@blog-app-shard-00-00.a197g.mongodb.net:27017,blog-app-shard-00-01.a197g.mongodb.net:27017,blog-app-shard-00-02.a197g.mongodb.net:27017/?ssl=true&replicaSet=atlas-1372nt-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Blog-App`;
   try {
-    await mongoose.connect(URL, { useNewUrlParser: true }); // connection string which will return a promise 
-    console.log("Database connected 🎃")
+    await mongoose.connect(URL); // connection string which will return a promise 
+    console.log("Database connected 🍀")
   } catch (error) {
     console.log("Error with the Database",error)
   }
