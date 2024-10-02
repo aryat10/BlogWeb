f4bca3cc908bfe626c6bb5af8e129e17194a212e
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2024-10-03 02:36:55 +0530

    Update user-controlller.js

--- Server/Controller/user-controlller.js
+++ Server/Controller/user-controlller.js
@@ -1,9 +1,11 @@
 import User from "../Model/user.js";
-
+import bcrypt from 'bcrypt'
 export const SignUser = async (req, res) => {
   // req handles all the request from params or the body or the API , res act as response which needs to be send from backend to frontend like the status of the ongoing work
   try {
-    const user = req.body;
+    // const salt = await bcrypt.getSalt()
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const user = {username: req.body.username, name: req.body.name, password: hashedPassword}
     const newUser = new User(user);
     await newUser.save();
 
